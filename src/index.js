4ae3abc974fd9fa2bd0b715acbb5d52ee8eb5702
H5misi <hazm.sayed.m@gmail.com>
2024-10-05 02:48:45 +0300

    Docker with MongoDB & Node JS

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
-const express = require("express");
+const express = require('express');
+const { default: mongoose } = require('mongoose');
 
 // init app
 const port = process.env.port || 4000;
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1>Hello! prod<h1>"));
+// connect to DB
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = 27017;
+const DB_HOST = 'mongo'; //host name must be same as service name in docker-compose.yml
+
+const URI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+    .connect(URI)
+    .then(() => console.log('connected'))
+    .catch((err) => console.log('failed to connect: ' + err));
+
+app.get('/', (req, res) => res.send('<h1>Hello! dev<h1>'));
 
 // Use backticks for template literals in the console log
 app.listen(port, () => console.log(`app is running on port: ${port}`));
